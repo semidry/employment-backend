12feedf3c40ce74b9b484b7deea1366a84053812
mxy <13614169547@163.com>
2023-10-12 22:51:34 +0800

    初始提交

---
+++ src/main/java/org/excellence/yunnanemploymentbackend/YunNanEmploymentBackendApplication.java
@@ -0,0 +1,13 @@
+package org.excellence.yunnanemploymentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunNanEmploymentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunNanEmploymentBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/excellence/yunnanemploymentbackend/YunNanEmploymentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.excellence.yunnanemploymentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunNanEmploymentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
