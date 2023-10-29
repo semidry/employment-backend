2d46b4c9bceb4331d1345e5dfe4937ab51ddaea6
mxy <13614169547@163.com>
2023-10-29 21:03:31 +0800

    完成UserController

--- src/main/java/org/excellence/yunnanemploymentbackend/controller/UserController.java
+++ src/main/java/org/excellence/yunnanemploymentbackend/controller/UserController.java
@@ -1,10 +1,37 @@
 package org.excellence.yunnanemploymentbackend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.excellence.yunnanemploymentbackend.entity.Response;
+import org.excellence.yunnanemploymentbackend.entity.User;
+import org.excellence.yunnanemploymentbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping(path = "user")
 public class UserController {
-    
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(path = "/login")
+    public @ResponseBody Response<Boolean> login(@RequestBody User user) {
+        final var result = userService.login(user);
+        if (!result.equals("success")) {
+            // response 的success指的是"响应是否成功", 不针对任何请求, 前端要的值在data中
+            return new Response<>(true, result, false);
+        }
+        return new Response<>(true, result, true);
+    }
+
+    @PostMapping(path = "register")
+    public @ResponseBody Response<Boolean> register(@RequestBody User user) {
+        final var result = userService.register(user);
+        if (!result.equals("success")) {
+            return new Response<>(true, result, false);
+        }
+        return new Response<>(true, result, true);
+    }
 }
